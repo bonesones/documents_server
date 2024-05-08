672a63bced790709496badb4d00a3666ed2f8606
Dmitriy <dmitriyworkspace013@gmail.com>
2024-05-08 14:14:55 +0300

    PORR edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors")
 const authRouter = require('./router/authRouter')
-const PORT = process?.env?.PORT || 5000
+const PORT = 5000
 
 const app = express();
 
